bb8637507290ec03ae604a8ce2123029ece4e1a3
ioreka <lucymhodges@gmail.com>
2018-07-24 08:30:43 +0100

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,55 @@ const questions = [
 ]
 
 let question;
+const questionContainer = document.querySelector('.question-container')
+
+function appendQuestion(question) {
+  questionContainer.innerText = question.questionText
+}
+
+function askQuestionThen(time){
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise((resolve, reject) => {
+    setTimeout(function(){
+      resolve(question)
+    })
+  })
+}
+
+function removeQuestion(){
+  return new Promise((resolve ,reject) => {
+    questionContainer.innerText = ''
+  })
+}
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+}
+
+// <div class="section center-align true-false-list">
+//   <div class="btn center-align hide  green lighten-2"> True </div>
+//   <div class="btn center-align hide red lighten-2"> False </div>
+// </div>
+
+function trueAndFalseButtons() {
+  let buttons = document.querySelector('.true-false-list').querySelectorAll('.btn')
+  return buttons
+}
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function(btn){
+    if (btn.classList.contains('hide')){
+      btn.classList.remove('hide')
+    } else {
+      btn.classList.add('hide')
+    }
+  })
+}
+
+function displayQuestionOnClick() {
+  let click = document.querySelector('.waves-light')
+  return click.addEventListener('click', () => {
+    askQuestionThenRemoveQuestion(2000)
+  })
+}
